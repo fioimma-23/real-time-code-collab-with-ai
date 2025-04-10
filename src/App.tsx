fb95bcc93350660741bf75fb8c6d4ae84aab74b5
fioimma23 <fioimma2308@gmail.com>
2025-04-10 10:42:52 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Sample from './sample'; // ← make sure this path is correct
+
+function App() {
+  return (
+    <div>
+      <Sample />
+    </div>
+  );
+}
+
+export default App;
